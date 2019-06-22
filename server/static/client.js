c47f83d42c02c4cf8d753dd5d87b846a7d848dc7
robidev <robin.dev@gmail.com>
2019-06-22 20:54:42 +0200

    added socket.io methods

--- server/static/client.js
+++ server/static/client.js
@@ -1,4 +1,4 @@
-var nodes, edges, network;
+var nodes, edges, network, socket;
 
 $(document).ready(function() {
 	//add tabs
@@ -7,31 +7,54 @@ $(document).ready(function() {
 		showCloseBtn : true,
 	});
 
-  /*$('#stopStreamButtonID').click(function(event) {        
-    socket.emit('stopStreamButtonID', {data: ''});
-  });
-
   namespace = '';
+  socket = io.connect('http://' + document.domain + ':' + location.port + namespace);
 
-  var socket = io.connect('http://' + document.domain + ':' + location.port + namespace);
-  socket.on('connect', function() {
-      socket.emit('my event', {data: 'Connected to server'});
+  /* register events from server */
+
+  socket.on('info_event', function (type, data) {
+    //event gets called from server when info data is updated, so update the info tab
+    //type = {ps, syslog, netstat};
+    //write data
+    $('#ps')[0].innerHTML = "";
+    $('#syslog')[0].innerHTML = "";
+    $('#netstat')[0].innerHTML = "";
   });
 
-  socket.on('serverUpdate', function(msg) {
-    //console.log(msg);
-    setStatus(msg);				
+  socket.on('log_event', function (host, data, clear) {
+    //event gets called from server when new log events are generated, and add them to the log tab
+    //if clear is set, all log data is cleared before adding the new data
+    //if host is not known, add the host
   });
-  socket.on('lastTrialDiv', function(msg) {
-    console.log('lastTrialDiv received')
-    plotSpinner(0);
-    $('#lastTrialPlotID').html(msg.plotDiv);				
+
+  socket.on('select_tab_event', function (host_index) {
+    //event gets called from server when the tab-focus should be changed
+    var tab = $('#hostlogtab.tabs');
+    var len = $('#hostlogtab.tabs')[0].children.length;
+    var href = $('#hostlogtab.tabs')[0].children[host_index].children[0];
+    var event = $.Event("click");
+    event.data = {ahref:"",tab:""};
+    event.data.ahref = href;
+    event.data.tab = tab;
+    $("#hostlogtab").showTab(event);
   });
-  socket.on('refreshvideostream', function(msg) {
-    console.log('refreshvideostream received')
-    setStatus(msg);				
-    window.location.reload()
-  });*/
+
+  socket.on('add_hosts_event', function (hosts, edges, apearance) {
+    //event gets called from server when new hosts are discovered, and should be added to the vis.js diagram
+    addNode();
+    addEdge();
+    addNewStaticTab();
+  });
+
+  socket.on('update_hosts_event', function (hosts, apearance) {
+    //event gets called from server when the host apearance should be updated in the vis.js diagram
+    updateNode();
+  });
+
+  socket.on('remove_hosts_event', function (hosts) {
+    //event gets called from server when hosts should be removed from the vis.js diagram
+  });
+
 });
 
 function addNewStaticTab()
@@ -114,22 +137,36 @@ function draw() {
 
 function add_host() {
 
-  addNewStaticTab();
-  var tab = $('#hostlogtab.tabs');
-
-  var len = $('#hostlogtab.tabs')[0].children.length;
-  var href = $('#hostlogtab.tabs')[0].children[len-1].children[0];
-  var event = $.Event("click");
-  event.data = {ahref:"",tab:""};
-  event.data.ahref = href;
-  event.data.tab = tab;
-  $("#hostlogtab").showTab(event);
-
-  $('#ps')[0].innerHTML = "";
-  $('#syslog')[0].innerHTML = "";
-  $('#netstat')[0].innerHTML = "";
-
-  addNode();
-  addEdge();
-  updateNode();
 }
+
+/********************************************************/
+/*             socket.io calls               */
+/********************************************************/
+
+/* emit from client to server */
+
+function get_page_data() {
+  socket.emit('get_page_data', {data: ''});
+  //call server to tell we want all data, so we can fill the ui (normally done once on full page load/refresh)
+}
+
+function get_hosts() {
+  socket.emit('get_hosts', {data: ''});
+  //call server to tell we want current list of hosts
+}
+
+function get_logging_data(host) {
+  socket.emit('get_logging_data', {data: ''});
+  //call server to tell we want last (100) loglines from 'host'
+}
+
+function get_info_data(host) {
+  socket.emit('get_info_data', {data: ''});
+  //call server to tell we want updated info tab from 'host'
+}
+
+function set_focus(host) {
+  socket.emit('set_focus', {data: ''});
+  //call server to tell we want to change focus, to receive updated info from 'host'
+}
+
